30d3c3ad7a95850ba293d60504ffa482fca03e31
Arex360 <49303647+Arex360@users.noreply.github.com>
2023-01-11 06:28:57 -0800

    update

--- routes/postImage.js
+++ routes/postImage.js
@@ -4,19 +4,20 @@ const query = require('../service/query')
 const fs = require('fs')
 const router = express.Router()
 router.post('/postImage',(req,res)=>{
-    let url = req.body.base64
-    let filename = ""
-    let client = req.body.client
-    
-    //filename = require('crypto').createHash('sha256').update(url).digest('hex').toString()
-    filename = "hsdjhsdjksdhjksdhjhdjsh"
-    filename = filename.substring(0,8)
-    let id= req.body.id
-    let output = filename+"out"
-    let base64 = url
-    filename = "images/"+filename +'.png'
-    fs.writeFile(filename,url,{encoding: 'base64'},err=>{
-        res.send('done')
-    })
+    let url = req.body.base64;
+    let filename = "";
+    let client = req.body.client;
+
+    // Create a unique filename
+    filename = require('crypto').createHash('sha256').update(url).digest('hex').toString();
+    filename = filename.substring(0, 8);
+    let id = req.body.id;
+    let output = filename + "out";
+    filename = "images/" + filename + '.png';
+    // Decode the base64 encoded image data
+    let binaryData = Buffer.from(url, 'base64');
+
+    fs.writeFileSync(filename, binaryData);
+    res.send('done')
 })
 module.exports = router

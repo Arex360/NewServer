4a42cd58a9e8916cc5767494d870c133bbb716da
Arex360 <49303647+Arex360@users.noreply.github.com>
2023-01-11 06:07:27 -0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const cluster = require('cluster')
 const cpus = require('os').cpus.length
 const process = require('process')
@@ -7,13 +8,13 @@ const {entry,getImage,postImage,addTrap,getTrap} = require('./routes/routes')
 const {connection} = require('./service/connection')
 let startServer = ()=>{
     const app = express()
+    app.use(cors())
     app.use(bodyparser.urlencoded({limit:'50mb',extended:false}))
     app.use(postImage)
     app.use(getImage)
     app.use(addTrap)
     app.use(getTrap)
     app.use(entry)
-    app.get('/',(req,res)=>res.send("ok"))
     //connection.connect()
     app.listen(5000,()=>console.log('server started'))
 }
--- routes/entry.js
+++ routes/entry.js
@@ -2,6 +2,7 @@ const express = require('express')
 const query = require('../service/query')
 const router = express.Router()
 router.get('/',(req,res)=>{
-    query("select * from photos",res)
+    //query("select * from photos",res)
+    res.send('OK')
 })
 module.exports = router

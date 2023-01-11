3dc7386ade74c5689a570569b5b9d62c8125264f
Arex360 <49303647+Arex360@users.noreply.github.com>
2023-01-11 06:26:07 -0800

    update

--- routes/postImage.js
+++ routes/postImage.js
@@ -15,17 +15,8 @@ router.post('/postImage',(req,res)=>{
     let output = filename+"out"
     let base64 = url
     filename = "images/"+filename +'.png'
-    fs.writeFile(filename,base64,{encoding: 'base64'},err=>{
+    fs.writeFile(filename,base64.toString(),{encoding: 'base64'},err=>{
         res.send('done')
     })
-    /*fs.writeFile(filename, base64, {encoding: 'base64'}, function(err) {
-        exec(`python detect.py --source ${filename} --weights best_new.pt --out ${output}`,()=>{
-            const url = `http://121.52.158.157:4000/img&${output}`
-            try{
-              query(`insert into photos (image,client) values ('${url}','${client}')`,res)
-            }catch(e){
-            }
-        })
-    });*/
 })
 module.exports = router

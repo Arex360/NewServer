19168ad0dec0dfaa172aa993e68b6636a2015af6
Arex360 <122282190+arex520@users.noreply.github.com>
2023-01-23 23:38:34 -0800

    update

--- utility/firebase_modified.cpp
+++ utility/firebase_modified.cpp
@@ -15,8 +15,6 @@ void send(std::string clientID,std::string tempreture,std::string humidity,std::
      CURLcode res;
      curl = curl_easy_init();
     if(curl) {
-        //std::string body = "{\"client\":\"" + clientID + "\",\"humidity\":\"" + humidity + "\",\"tempreture\":\"" + tempreture + "\",\"voltage\":\"" + voltage + "\",\"current\":\"" + current + "\"}";
-        //std::string body = "{\"client\":\"" + clientID + "\",\"humidity\":\"" + humidity + "\",\"temperature\":\"" + tempreture + "\",\"voltage\":\"" + std::to_string(voltage) + "\",\"current\":\"" + std::to_string(current) + "\"}";
         std::string body = "{\"client\":\"" + clientID + "\",\"humidity\":\"" + humidity + "\",\"tempreture\":\"" + tempreture + "\",\"voltage\":\"" + voltage + "\",\"current\":\"" + current + "\"}";
 
         struct curl_slist *headers = NULL;

d81179440ee8509971d56731ace633650461c149
Arex360 <122282190+arex520@users.noreply.github.com>
2023-01-23 19:58:49 -0800

    update

--- newML/app.py
+++ newML/app.py
@@ -51,4 +51,4 @@ if __name__ == '__main__':
     def detect_image():
         process_img()
         return 'Hello, World!'
-    app.run(host='0.0.0.0', port=443)
+    app.run(host='0.0.0.0', port=80)

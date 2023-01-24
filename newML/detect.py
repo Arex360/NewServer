736e9b2b761409e998b62ff09d81a0b351be3e71
Arex360 <122282190+arex520@users.noreply.github.com>
2023-01-23 20:13:37 -0800

    update

--- newML/detect.py
+++ newML/detect.py
@@ -141,7 +141,7 @@ def detect(save_img=False):
             if save_img:
                 if dataset.mode == 'image':
                     cv2.imwrite(save_path, im0)
-                    printf(f"the absolute path is {os.path.abspath(save_path)}")
+                    print(f"the absolute path is {os.path.abspath(save_path)}")
                     print(f" The image with the result is saved in: {save_path}")
                 else:  # 'video' or 'stream'
                     if vid_path != save_path:  # new video

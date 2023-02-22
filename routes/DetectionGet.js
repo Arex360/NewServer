c8f74ef628f9c70ad601a6967d40c3867f83f3a8
Arex360 <122282190+arex520@users.noreply.github.com>
2023-02-22 06:57:01 +0500

    update

--- routes/DetectionGet.js
+++ routes/DetectionGet.js
@@ -5,6 +5,9 @@ const router = express.Router()
 router.get('/Getdetection/:clientID/:name', async (req,res)=>{
     let {clientID,name} = req.params
     const snapShot = await getDetection({id:clientID,name})
-    res.send(snapShot.count)
+    if(snapShot)
+        res.send(snapShot.count)
+    else 
+        res.send("0")
 })
 module.exports = router

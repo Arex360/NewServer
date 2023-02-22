7a1df851db8e65122d1a62d0b2037ee6a540b2f5
Arex360 <122282190+arex520@users.noreply.github.com>
2023-02-22 06:54:09 +0500

    update

--- routes/DetectionGet.js
+++ routes/DetectionGet.js
@@ -4,7 +4,7 @@ const getDetection = require('../firebase/getDetection')
 const router = express.Router()
 router.get('/Getdetection/:clientID/:name', async (req,res)=>{
     let {clientID,name} = req.params
-    const snapShot = getDetection({id:clientID,name})
-    res.send(snapshot)
+    const snapShot = await getDetection({id:clientID,name})
+    res.send(snapShot)
 })
 module.exports = router

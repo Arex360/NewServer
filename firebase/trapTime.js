ddc98f62b3b94c8e69a2f0077034e2a5a68310b0
Arex360 <122282190+arex520@users.noreply.github.com>
2023-03-03 15:58:21 +0500

    update

--- firebase/trapTime.js
+++ firebase/trapTime.js
@@ -1,7 +1,9 @@
 const {database} = require('./firebase')
 const getTrapTime = async ({trapID})=>{
     let ref = database.ref(`env/${trapID}/date`)
+    let time = database.ref(`env/${trapID}/timestamp`)
     const snapshot = await ref.get()
-    return snapshot.val()
+    const _time = await time.get()
+    return snapshot.val() + '-'+ _time.val()
 }
 module.exports = getTrapTime

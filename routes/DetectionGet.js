d9f4d8eeade772ef8d4960817f004918fa044210
Arex360 <122282190+arex520@users.noreply.github.com>
2023-07-08 09:20:13 +0500

    update

--- routes/DetectionGet.js
+++ routes/DetectionGet.js
@@ -4,6 +4,12 @@ const getDetection = require('../firebase/getDetection')
 const router = express.Router()
 router.get('/Getdetection/:clientID/:name', async (req,res)=>{
     let {clientID,name} = req.params
+    if(name == "fallarmyworm")
+    {
+        name = "fall-armyworm"
+    }else if( name == "pinkbollworm"){
+        name = "pink-bollworm"
+    }
     const snapShot = await getDetection({id:clientID,name})
     if(snapShot)
         res.send(snapShot.count)

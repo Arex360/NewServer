c4034a101c39a70f015e63499c6bc0181afa0db9
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-01-28 21:17:41 +0500

    update

--- job.js
+++ job.js
@@ -1,119 +1,136 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const path = require('path');
-const bodyparser = require('body-parser');
-const fs = require('fs');
-const crypto = require('crypto');
-
-class ImageHandler {
-    constructor() {
-        this.clientImageData = {};
-    }
-
-    async processImage(url, client, flag) {
-        console.log(flag);
-        let filename = '';
-        let log = new Date().toString() + ' received image\n';
-        fs.appendFile(client + '.txt', log, (err) => {
-            if (err) throw err;
-            console.log('Request logged');
-        });
-
-        // Create a unique filename
-        filename = crypto
-            .createHash('sha256')
-            .update(client + Date.now().toString())
-            .digest('hex')
-            .toString()
-            .substring(0, 8);
-        let output = filename + 'out';
-        filename = 'images/' + client + '_' + filename + '.png';
-
-        if (flag !== 2) {
-            if (!this.clientImageData[client] && flag === 0) {
-                this.clientImageData[client] = [];
-            }
-            if (!this.clientImageData[client] || this.clientImageData[client] === undefined) {
-                this.clientImageData[client] = [];
-            }
-            this.clientImageData[client].push(url);
-            console.log(this.clientImageData[client].length);
-        } else {
-            // Check if this.clientImageData[client] is defined before accessing its properties
-            console.log("wait...")
-            if (true) {
-                console.log("writting image")
-                let totalData = '';
-                for (let i = 0; i < this.clientImageData[client].length; i++) {
-                    totalData += this.clientImageData[client][i];
-                }
-                // Decode the base64 encoded image data
-                let binaryData = Buffer.from(totalData, 'base64');
-                // Check if the file size is greater than 100KB
-                // 100000
-                if (binaryData.length > 100) {
-                    fs.writeFileSync(filename, binaryData);
-                    const absPath = path.resolve(filename);
-                    console.log(absPath);
-                    // Clear the image data for this client
-                    this.clientImageData[client] = null;
-                    console.log(filename);
-
-                    let res = await axios.get(`http://127.0.0.1:5000/getmodel/${client}`);
-                    res = res.data;
-                    res = res.modelID;
-                    console.log(`printing model : ${res}`);
-                    //axios.post("http://127.0.0.1:80", { path: absPath, client, model: res });
-                }
-                const date = Date.now() / 1000;
-                console.log('getting date');
-                let data = '';
-                if (client !== 'date') {
-                    data = await axios.get(`http://localhost:5000/Adddetection/${client}/date/${date}`);
-                }
-            }
-        }
-    }
-}
-
-const app = express();
-app.use(cors());
+const express = require('express')
+const cors = require('cors')
+const base64ToImage = require("base64-to-image");
+const axios = require("axios");
+const path = require('path')
+const bodyparser = require('body-parser')
+const fs = require('fs')
+let clientImageData = {};
+const app = express()
+const crypto = require('crypto')
+app.use(cors())
+//app.use(bodyparser());
 app.use(bodyparser.json({ limit: '50mb', extended: true }));
 app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }));
-
-const imageHandler = new ImageHandler();
-const queue = [];
-
-const ExecuteQueue = async () => {
-    setTimeout(() => {
-        if (queue.length > 0) {
-            let i = queue.splice(0, 1);
-            //i[0]['out']()
-            imageHandler.processImage(i[0]['out'].url, i[0]['out']._client, i[0]['out'].flag);
-            console.log('done');
-        }
-        setTimeout(() => ExecuteQueue(), 800);
-    }, 2000);
-};
-
-app.get('/exp/:data', (req, res) => {
-    queue.push();
-    res.send('ok');
-});
-
-app.post('/postImagePart/:client/:flag', async (req, res) => {
+const queue = []
+let keys0 = {}
+let keys1 = {}
+let keys2 = {}
+let keys3 = {}
+let keys4 = {}
+let keys5 = {}
+let keys6 = {}
+let keys7 = {}
+let template = async (req,res,id)=>{
+    let keys = {}
+    if(id==0)
+        keys = keys0
+    else if(id == 1)
+        keys = keys1
+        else if(id == 2)
+        keys = keys2
+        else if(id == 3)
+        keys = keys3
+        else if(id == 4)
+        keys = keys4
+        else if(id == 5)
+        keys = keys5
+        else if(id == 6)
+        keys = keys6
+    
     let url = req.body.base64;
-    console.log(url.length);
-    let flag = req.params.flag;
-    let _client = req.params.client;
-    console.log(`Registering ${_client} with ${flag}`)
-    // queue.push({'out':()=>build(url,_client,flag,0)})
-    queue.push({ 'out': { url, _client, flag } });
-    res.send('done');
-});
+    let flag = req.params.flag
+    console.log(flag)
+    let filename = "";
+    let {client} = req.params
+    let log = new Date().toString() + "received image" +"\n"
+    fs.appendFile(client+'.txt', log, (err) => {
+      if (err) throw err;
+      console.log('Request logged');
+    });
+    // Create a unique filename
+    filename = require("crypto")
+      .createHash("sha256")
+      .update(client+Date.now().toString())
+      .digest("hex")
+      .toString();
+    filename = filename.substring(0, 8);
+    let id = req.body.id;
+    let output = filename + "out";
+    filename = "images/" + client + "_" +filename + ".png";
+    if(flag != 2){
+       if(keys[client] == null && flag == 0){
+          keys[client] = []
+       }
+       keys[client].push(url)
+       console.log(keys[client].length)
+    }else{
+      let totalData = ""
+      for(let i = 0; i < keys[client].length;i++){
+          totalData += keys[client][i]
+      }
+        // Decode the base64 encoded image data
+    let binaryData = Buffer.from(totalData, "base64");
+    // Check if the file size is greater than 100KB
+    // 100000
+    if (binaryData.length > 100) {
+      fs.writeFileSync(filename, binaryData);
+      const absPath =path.resolve(filename)
+      console.log(absPath)
+      keys[client] = null
+      console.log(filename)
 
-app.listen(1000, () => {
-    console.log('starting..');
-    ExecuteQueue();
-});
+    
+      let res = await axios.get(`http://127.0.0.1:5000/getmodel/${client}`)
+      res = res.data
+      res = res.modelID
+      console.log(`printing model : ${res}`)
+      axios.post("http://127.0.0.1:80", { path: absPath, client , model:res});
+    }
+    const date = Date.now()/1000
+    console.log("getting date")
+    let data = ""
+    if(client != "date")
+       data = await axios.get(`http://localhost:5000/Adddetection/${client}/date/${date}`)
+    }
+    if(id==0)
+    keys0 = keys
+else if(id == 1)
+    keys1 = keys
+    else if(id == 2)
+    keys2 = keys
+    else if(id == 3)
+    keys3 = keys
+    else if(id == 4)
+    keys4 = keys
+    else if(id == 5)
+    keys5 = keys
+    else if(id == 6)
+    keys6 = keys
+    res.send("done");
+
+}
+app.post("/postImagePart0/:client/:flag",async(req,res)=>{
+    template(req,res,0)
+})
+app.post("/postImagePart1/:client/:flag",async(req,res)=>{
+    template(req,res,1)
+})
+app.post("/postImagePart2/:client/:flag",async(req,res)=>{
+    template(req,res,2)
+})
+app.post("/postImagePart3/:client/:flag",async(req,res)=>{
+    template(req,res,3)
+})
+app.post("/postImagePart4/:client/:flag",async(req,res)=>{
+    template(req,res,4)
+})
+app.post("/postImagePart5/:client/:flag",async(req,res)=>{
+    template(req,res,5)
+})
+app.post("/postImagePart6/:client/:flag",async(req,res)=>{
+    template(req,res,6)
+})
+app.listen(1000,()=>{
+    console.log("starting..")
+})

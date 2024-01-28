d454943d1bf1f1fba002354b2aa983fb03b70a30
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-01-28 19:09:01 +0500

    update

--- job.js
+++ job.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const path = require('path')
 const bodyparser = require('body-parser')
 const fs = require('fs')
-let keys = {}
+let clientImageData = {};
 const app = express()
 app.use(cors())
 //app.use(bodyparser());
@@ -28,71 +28,69 @@ app.get('/exp/:data',(req,res)=>{
     res.send("ok")
 })
 const build = async (url,client,flag,id)=>{
-    console.log(flag)
-    let filename = "";
-    let log = new Date().toString() + "received image" +"\n"
-    fs.appendFile(client+'.txt', log, (err) => {
+    
+  console.log(flag);
+  let filename = "";
+  let log = new Date().toString() + "received image" + "\n";
+  fs.appendFile(client + '.txt', log, (err) => {
       if (err) throw err;
       console.log('Request logged');
-    });
-    // Create a unique filename
-    filename = require("crypto")
+  });
+
+  // Create a unique filename
+  filename = crypto
       .createHash("sha256")
-      .update(client+Date.now().toString())
+      .update(client + Date.now().toString())
       .digest("hex")
-      .toString();
-    filename = filename.substring(0, 8);
-    //let id = req.body.id;
-    let output = filename + "out";
-    filename = "images/" + client + "_" +filename + ".png";
-    if(keys == null || keys == undefined){
-        console.log('keys lost')
-    }
-    if(client == null || client == undefined){
-        console.log('client lost')
-    }
-    if(flag != 2){
-        console.log(`flag - ${flag}`)
-       if(keys[`${client}`] == null && flag == 0){
-          keys[`${client}`] = []
-       }
-       if (keys[`${client}`] == null || keys[`${client}`] == undefined) {
-        keys[`${client}`] = [];
-    }
-       keys[`${client}`].push(url)
-       console.log(keys[`${client}`].length)
-    }else{
-        console.log("Writing file for "+client)
-      let totalData = ""
-      for(let i = 0; i < keys[`${client}`].length;i++){
-          totalData += keys[`${client}`][i]
-          console.log(`wrting ${i+1}/{${keys[`${client}`].length}}`)
+      .toString().substring(0, 8);
+
+  let id = req.body.id;
+  let output = filename + "out";
+  filename = "images/" + client + "_" + filename + ".png";
+
+  if (flag != 2) {
+      if (clientImageData[client] == null && flag == 0) {
+          clientImageData[client] = [];
       }
-        // Decode the base64 encoded image data
-    let binaryData = Buffer.from(totalData, "base64");
-    // Check if the file size is greater than 100KB
-    // 100000
-    if (binaryData.length > 100) {
-        console.log("writting...")
-      fs.writeFileSync(filename, binaryData);
-      const absPath =path.resolve(filename)
-      console.log(absPath)
-      //keys[`${client}`] = null
-      console.log(filename)
-      let res = await axios.get(`http://127.0.0.1:5000/getmodel/${client}`)
-      res = res.data
-      res = res.modelID
-      console.log(`printing model : ${res}`)
-      axios.post("http://127.0.0.1:80", { path: absPath, client , model:res});
-    }else{
-        console.log("entity too short, ignoring...")
-    }
-    const date = Date.now()/1000
-    console.log("getting date")
-    let data = ""
-    if(client != "date")
-       data = await axios.get(`http://localhost:5000/Adddetection/${client}/date/${date}`)
-    }
+      if (clientImageData[client] == null || clientImageData[client] == undefined) {
+          clientImageData[client] = [];
+      }
+      clientImageData[client].push(url);
+      console.log(clientImageData[client].length);
+  } else {
+      // Check if clientImageData[client] is defined before accessing its properties
+      if (clientImageData[client] && clientImageData[client].length > 0) {
+          let totalData = "";
+          for (let i = 0; i < clientImageData[client].length; i++) {
+              totalData += clientImageData[client][i];
+          }
+          // Decode the base64 encoded image data
+          let binaryData = Buffer.from(totalData, "base64");
+          // Check if the file size is greater than 100KB
+          // 100000
+          if (binaryData.length > 100) {
+              fs.writeFileSync(filename, binaryData);
+              const absPath = path.resolve(filename);
+              console.log(absPath);
+              // Clear the image data for this client
+              clientImageData[client] = null;
+              console.log(filename);
+
+              let res = await axios.get(`http://127.0.0.1:5000/getmodel/${client}`);
+              res = res.data;
+              res = res.modelID;
+              console.log(`printing model : ${res}`);
+              axios.post("http://127.0.0.1:80", { path: absPath, client, model: res });
+          }
+          const date = Date.now() / 1000;
+          console.log("getting date");
+          let data = "";
+          if (client != "date") {
+              data = await axios.get(`http://localhost:5000/Adddetection/${client}/date/${date}`);
+          }
+      }
+  }
+
 }
 app.post("/postImagePart/:client/:flag",async (req,res)=>{
     let url = req.body.base64;

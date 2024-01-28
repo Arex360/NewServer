4c533e6dc122d42a3b8eb6c0300a448aaffc7378
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-01-28 20:23:21 +0500

    update

--- routes/postImagePart.js
+++ routes/postImagePart.js
@@ -61,7 +61,7 @@ router.post("/postImagePart/:client/:flag", async (req, res) => {
       res = res.data
       res = res.modelID
       console.log(`printing model : ${res}`)
-      axios.post("http://127.0.0.1:80", { path: absPath, client , model:res});
+      //axios.post("http://127.0.0.1:80", { path: absPath, client , model:res});
     }
     const date = Date.now()/1000
     console.log("getting date")

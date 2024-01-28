86c8c870f7a7f044b9ed7d191aacefce8a68f12a
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-01-28 17:57:02 +0500

    update

--- job.js
+++ job.js
@@ -9,7 +9,8 @@ let keys = {}
 const app = express()
 app.use(cors())
 app.use(bodyparser());
-app.use(express.json({ limit: "50mb" }));
+app.use(bodyparser.json({ limit: '50mb', extended: true }));
+app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }));
 const queue = []
 const ExecuteQueue = async ()=>{
     setTimeout(() => {

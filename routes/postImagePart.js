e5ce62eeb0f527c2febcdcdad1aabae8a9bc1c05
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-01-28 18:52:49 +0500

    update

--- routes/postImagePart.js
+++ routes/postImagePart.js
@@ -15,10 +15,62 @@ const axios = require("axios");
 const path = require('path')
 let keys = {}
 router.post("/postImagePart/:client/:flag", async (req, res) => {
-  let base64 = req.body.base64;
-  console.log(base64)
-  axios.post(`http://localhost:1000/postImagePart/${req.params.client}/${req.params.flag}`,{base64})
-  res.send("ok")    
-});
+    let url = req.body.base64;
+    let flag = req.params.flag
+    console.log(flag)
+    let filename = "";
+    let {client} = req.params
+    let log = new Date().toString() + "received image" +"\n"
+    fs.appendFile(client+'.txt', log, (err) => {
+      if (err) throw err;
+      console.log('Request logged');
+    });
+    // Create a unique filename
+    filename = require("crypto")
+      .createHash("sha256")
+      .update(client+Date.now().toString())
+      .digest("hex")
+      .toString();
+    filename = filename.substring(0, 8);
+    let id = req.body.id;
+    let output = filename + "out";
+    filename = "images/" + client + "_" +filename + ".png";
+    if(flag != 2){
+       if(keys[client] == null && flag == 0){
+          keys[client] = []
+       }
+       keys[client].push(url)
+       console.log(keys[client].length)
+    }else{
+      let totalData = ""
+      for(let i = 0; i < keys[client].length;i++){
+          totalData += keys[client][i]
+      }
+        // Decode the base64 encoded image data
+    let binaryData = Buffer.from(totalData, "base64");
+    // Check if the file size is greater than 100KB
+    // 100000
+    if (binaryData.length > 100) {
+      fs.writeFileSync(filename, binaryData);
+      const absPath =path.resolve(filename)
+      console.log(absPath)
+      keys[client] = null
+      console.log(filename)
+
+    
+      let res = await axios.get(`http://127.0.0.1:5000/getmodel/${client}`)
+      res = res.data
+      res = res.modelID
+      console.log(`printing model : ${res}`)
+      axios.post("http://127.0.0.1:80", { path: absPath, client , model:res});
+    }
+    const date = Date.now()/1000
+    console.log("getting date")
+    let data = ""
+    if(client != "date")
+       data = await axios.get(`http://localhost:5000/Adddetection/${client}/date/${date}`)
+    }
+    res.send("done");
+  });
 module.exports = router;
 

bececa20223fce69ce9f329121f664ae41ffb031
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-01-28 20:34:46 +0500

    update

--- routes/postImagePart.js
+++ routes/postImagePart.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const crypto = require("crypto");
-const query = require("../service/query");
 const fs = require("fs");
-const router = express.Router();
-const base64ToImage = require("base64-to-image");
 const axios = require("axios");
-const path = require('path')
+const path = require("path");
 
 let keys = {};
 let lock = {};
+
+const router = express.Router();
 
 router.post("/postImagePart/:client/:flag", async (req, res) => {
   const { client } = req.params;
   const url = req.body.base64;
   const flag = req.params.flag;
 
-  console.log(flag);
-
-  const log = new Date().toString() + "received image" + "\n";
-  fs.appendFile(client + '.txt', log, (err) => {
-    if (err) throw err;
-    console.log('Request logged');
-  });
+  console.log("Request logged");
+  console.log("client:", client);
+  console.log("flag:", flag);
 
   // Ensure that keys object is initialized for each client
   if (!keys[client]) {
@@ -33,12 +28,15 @@ router.post("/postImagePart/:client/:flag", async (req, res) => {
     lock[client] = true;
 
     try {
+      console.log("Entering critical section");
+
       // Your existing code for handling requests
-      if (flag !== 2) {
+      if (flag !== "2") {
         keys[client].push(url);
-        console.log(keys[client].length);
+        console.log("Pushed URL to keys array");
+        console.log("keys length:", keys[client].length);
       } else {
-        const totalData = keys[client].join('');
+        const totalData = keys[client].join("");
 
         if (totalData.length > 0) {
           const binaryData = Buffer.from(totalData, "base64");
@@ -54,32 +52,35 @@ router.post("/postImagePart/:client/:flag", async (req, res) => {
 
             fs.writeFileSync(imagePath, binaryData);
             const absPath = path.resolve(imagePath);
-            console.log(absPath);
-            console.log(imagePath);
+            console.log("Image saved:", absPath);
 
             const response = await axios.get(
               `http://127.0.0.1:5000/getmodel/${client}`
             );
             const modelID = response.data.modelID;
-            console.log(`printing model : ${modelID}`);
+            console.log("Model ID:", modelID);
+
             // Uncomment the following line if you want to send a POST request
             // await axios.post("http://127.0.0.1:80", { path: absPath, client, model: modelID });
           }
 
           if (client !== "date") {
             const date = Date.now() / 1000;
-            console.log("getting date");
+            console.log("Getting date:", date);
             const data = await axios.get(
               `http://localhost:5000/Adddetection/${client}/date/${date}`
             );
           }
         }
       }
+
+      console.log("Exiting critical section");
     } finally {
       // Release the lock
       delete lock[client];
     }
 
+    console.log("Done processing request");
     res.send("done");
   }
 });

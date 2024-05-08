649feab20addf5932c9248289bca36e28f7ab66d
Arex360 <49303647+Arex360@users.noreply.github.com>
2024-05-08 13:20:20 +0500

    update

--- Client/sendbase.js
+++ Client/sendbase.js
@@ -4,7 +4,7 @@ base64('pic.jpeg').then(res=>{
     console.log("took base64")
     const base64 = res
     const client = "client7"
-    axios.post('http://mnsstrap.ddns.net:5000/postImage/client21',{client,base64}).then(res=>{
+    axios.post('http://mnsstrap.ddns.net:5000/postImage/client11',{client,base64}).then(res=>{
         console.log('request sent')
     }).catch(e=>{
         console.log(e)
